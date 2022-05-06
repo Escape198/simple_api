2cdda0a2e1bfe00be01159dbb9c86ed30946d45f
Ruslan Kirzhanov <101140452+Escape198@users.noreply.github.com>
2022-05-06 17:19:05 +0300

    Update urls.py

--- bewise/main/urls.py
+++ bewise/main/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .api import QuestionsViewSet
 
 router = routers.DefaultRouter()
-router.register('api/questions', QuestionsViewSet, 'questions')
+router.register('api/main', QuestionsViewSet, 'questions')
 
 urlpatterns = router.urls
